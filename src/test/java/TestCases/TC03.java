17e10560b0b2f8052ab2231f888f49db9da0ddcf
selma <camselma2@gmail.com>
2021-11-07 19:11:41 -0500

    Initial commit

---
+++ src/test/java/TestCases/TC03.java
@@ -0,0 +1,37 @@
+package TestCases;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC03 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        String url="https://login2.nextbasecrm.com";
+        driver.get(url);
+
+        driver.findElement(By.xpath("//input[@name='USER_PASSWORD']")).sendKeys("hr36@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@name='USER_PASSWORD']")).sendKeys("UserUser"+ Keys.ENTER);
+        WebElement loginBtn= driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginBtn.click();
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "nextbasecrm";
+
+        System.out.println(actualTitle.equals(expectedTitle) ? "Pass": "Failed");
+
+
+
+
+    }
+}
+
+   /*Step 1. Open website
+Step 2. User should see "Email" in placeholder and type-in valid email address there
+Step 3. User should see "Password" in placeholder and type-in valid password there
+Step 4. Click on the "Log in" button*/
